7c6e810ea490552b50e12de17dee3d81cadb0152
AngelNavarroMesas <angel.navarro@iesnervion.es>
2023-10-31 08:34:41 +0100

    comiti

--- PiedraPapelTijerasJC/app/src/main/java/com/example/piedrapapeltijerasjc/MainActivity.kt
+++ PiedraPapelTijerasJC/app/src/main/java/com/example/piedrapapeltijerasjc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.piedrapapeltijerasjc
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -19,6 +20,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,50 +44,126 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//Estas son las variables:
+//estas dos son para identificar la eleccion del jugador y la maquina
+var jugador = 0
+var bot=0
+//Estas dos cuentan los puntos de cada uno
+var puntosJug = 0
+var puntosBot = 0
+//cuenta el turno por el que va la partida
+var partida = 0
+
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column (Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+    val context= LocalContext.current
 
+    Column (Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+        //Estas son las tres elecciones de la maquina, las he puesto en botones en caso de que haya que hacer una version pvp pero no hacen nada, es solo estetico
         Row (Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly){
-            Button(onClick = { /*TODO*/ },Modifier.width(width =125.dp).padding(5.dp)) {
+            Button(onClick = { /*TODO*/ },
+                Modifier
+                    .width(width = 125.dp)
+                    .padding(5.dp)) {
                 Image(painter = painterResource(id = R.drawable.piedra), contentDescription = "piedra")
             }
-            Button(onClick = { /*TODO*/ },Modifier.width(width =125.dp).padding(5.dp)) {
+            Button(onClick = { /*TODO*/ },
+                Modifier
+                    .width(width = 125.dp)
+                    .padding(5.dp)) {
                 Image(painter = painterResource(id = R.drawable.papel), contentDescription = "papel")
             }
-            Button(onClick = { /*TODO*/ },Modifier.width(width =125.dp).padding(5.dp)) {
+            Button(onClick = { /*TODO*/ },
+                Modifier
+                    .width(width = 125.dp)
+                    .padding(5.dp)) {
                 Image(painter = painterResource(id = R.drawable.tijeras), contentDescription = "tijeras")
             }
         }
 
-        Row (Modifier.height(400.dp).fillMaxWidth()){}
+        //Esta fila mostraria la eleccion de cada jugador pero no he conseguido hacer que funcione la funcion
+        Row (Modifier.height(400.dp).fillMaxWidth()){ /*mostrarEleccion()*/}
 
+        //Estas son las elecciones del jugador, cada una es un boton con una imagen indicando si es piedra, papel o tijeras
         Row (Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly){
-            Button(onClick = { jugar(1) },Modifier.width(width =125.dp).padding(5.dp)) {
+            Button(onClick = { jugador=1; turno(context) },
+                Modifier
+                    .width(width = 125.dp)
+                    .padding(5.dp)) {
                 Image(painter = painterResource(id = R.drawable.piedra), contentDescription = "piedra")
             }
-            Button(onClick = { /*TODO*/ },Modifier.width(width =125.dp).padding(5.dp)) {
+            Button(onClick = { jugador=2; turno(context) },
+                Modifier
+                    .width(width = 125.dp)
+                    .padding(5.dp)) {
                 Image(painter = painterResource(id = R.drawable.papel), contentDescription = "papel")
             }
-            Button(onClick = { /*TODO*/ },Modifier.width(width =125.dp).padding(5.dp)) {
+            Button(onClick = { jugador=3; turno(context) },
+                Modifier
+                    .width(width = 125.dp)
+                    .padding(5.dp)) {
                 Image(painter = painterResource(id = R.drawable.tijeras), contentDescription = "tijeras")
             }
         }
     }
+
 }
 
-fun jugar(jugador: Int){
-    var bot: Int = Random.nextInt()*3;
+//Esta es la funcion que deberia mostrar las imagenes en el centro
+//@Composable
+//fun mostrarEleccion() {
+//    if (jugador == 1){
+//        Image(painter = painterResource(id = R.drawable.piedra), contentDescription = "Elección 1", Modifier.width(width = 125.dp).padding(5.dp))
+//    }else if (jugador == 2){
+//        Image(painter = painterResource(id = R.drawable.papel), contentDescription = "Elección 2", Modifier.width(width = 125.dp).padding(5.dp))
+//    }else{
+//        Image(painter = painterResource(id = R.drawable.piedra), contentDescription = "Elección 3", Modifier.width(width = 125.dp).padding(5.dp))
+//    }
+//
+//    if (bot == 1){
+//        Image(painter = painterResource(id = R.drawable.piedra), contentDescription = "Máquina 1", Modifier.width(width = 125.dp).padding(5.dp))
+//    }else if (bot == 2){
+//        Image(painter = painterResource(id = R.drawable.papel), contentDescription = "Máquina 2", Modifier.width(width = 125.dp).padding(5.dp))
+//    }else{
+//        Image(painter = painterResource(id = R.drawable.tijeras), contentDescription = "Máquina 3", Modifier.width(width = 125.dp).padding(5.dp))
+//    }
+//}
+
+//Esta funcion muestra los resultados de cada turno y de la partida en general
+fun turno(context: Context){
+    //Hacemos que la maquina haga una eleccion aleatoria
+    bot= Random.nextInt(1,4);
     var texto: String="";
 
+    //Comprobamos quien ha ganado, en caso de empate no cuenta como que ha pasado un turno
     if(jugador==bot){
         texto="Empate"
-    }else if(jugador>bot){
+    }else if(jugador==1&&bot==3||jugador==2&&bot==1||jugador==3&&bot==2){
         texto="Has ganado"
-    }else if(jugador<bot){
+        puntosJug ++
+        partida++
+    }else if(jugador==3&&bot==1||jugador==1&&bot==2||jugador==2&&bot==3){
         texto="has perdido"
+        puntosBot ++
+        partida++
     }
-    var mensaje = Toast.makeText(this texto, Toast.LENGTH_SHORT) // in Activity
+
+    //comprobamos si se termina la partida, si han pasado 5 turnos termina y muestra quien es el ganador reiniciando los puntos de los dos jugadores y el contador de turnos
+    //y si no han pasado 5 turnos muestra el ganador de este turno
+    if(partida!=5){
+        Toast.makeText(context, texto, Toast.LENGTH_SHORT).show()
+    }else if(puntosJug>puntosBot){
+        Toast.makeText(context, "HAS GANADO LA PARTIDA!!!!  $puntosJug-$puntosBot", Toast.LENGTH_SHORT).show()
+        partida=0
+        puntosJug=0
+        puntosBot=0
+    }else{
+        Toast.makeText(context, "Has perdido la partida...  $puntosJug-$puntosBot", Toast.LENGTH_SHORT).show()
+        partida=0
+        puntosJug=0
+        puntosBot=0
+    }
 }
 
 @Preview(showBackground = true)
